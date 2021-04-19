7d2e5d77cf2128fd75a67aa4445be98b8339addc
dkolesnikov <trnlht@gmail.com>
2021-04-19 10:00:44 +0300

    Tests for BruteCollinearPoints added

--- Collinear/BruteCollinearPoints.java
+++ Collinear/BruteCollinearPoints.java
@@ -4,15 +4,23 @@
  *  Description:
  **************************************************************************** */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints
 {
     private int segmentsNum;
 
+    ArrayList<LineSegment> segmentsArr;
+
     public BruteCollinearPoints(Point[] points)
     {
+        if (points == null)
+            throw new IllegalArgumentException();
+
         segmentsNum = 0;
+
+        segmentsArr = new ArrayList<>();
 
         for (int i = 0; i < points.length; i++)
             for (int j = i + 1; j < points.length; j++)
@@ -27,7 +35,9 @@ public class BruteCollinearPoints
                         {
                             Point[] segment = { points[i], points[j], points[k], points[m] };
                             Arrays.sort(segment);
-                            LineSegment ls = new LineSegment(points[0], points[3]);
+                            LineSegment ls = new LineSegment(segment[0], segment[3]);
+
+                            segmentsArr.add(ls);
 
                             segmentsNum++;
                         }
@@ -37,16 +47,35 @@ public class BruteCollinearPoints
 
     public int numberOfSegments()
     {
-
+        return segmentsNum;
     }
 
     public LineSegment[] segments()
     {
-
+        LineSegment[] ret = new LineSegment[segmentsArr.size()];
+        return segmentsArr.toArray(ret);
     }
 
     public static void main(String[] args)
     {
+        Point[] points = {
+                new Point(0, 0),
+                new Point(0, 1),
+                new Point(0, 2),
+                new Point(0, 3),
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(1, 2),
+                new Point(1, 3)
+        };
+
+        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
+
+        System.out.println("Segments count: " + bcp.numberOfSegments());
+        System.out.println("Segments:");
+        LineSegment[] segs = bcp.segments();
+        for (int i = 0; i < bcp.numberOfSegments(); i++)
+            System.out.println(segs[i].toString());
 
     }
 }
